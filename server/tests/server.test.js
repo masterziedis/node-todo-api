b9a68feb4a26a566e7657d53f649136e328f7536
Vaidotas Kaukėnas <masterziedis@hotmail.com>
2017-07-29 14:43:35 +0300

    Add tests for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -8,8 +8,18 @@ const {
   Todo
 } = require('./../models/todo');
 
+const todos = [{
+    text: 'First test todo'
+  },
+  {
+    text: 'Second test todo'
+  }
+];
+
 beforeEach((done) => {
-  Todo.remove({}).then(() => done())
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos);
+  }).then(() => done())
 });
 
 describe('POST /todos', () => {
@@ -30,7 +40,9 @@ describe('POST /todos', () => {
           return done(err);
         }
 
-        Todo.find().then((todos) => {
+        Todo.find({
+          text
+        }).then((todos) => {
           expect(todos.length).toBe(1);
           expect(todos[0].text).toBe(text);
           done();
@@ -48,9 +60,21 @@ describe('POST /todos', () => {
         }
 
         Todo.find().then((todos) => {
-          expect(todos.length).toBe(0);
+          expect(todos.length).toBe(2);
           done();
         }).catch((e) => done(e))
       });
+  });
+});
+
+describe('GET /todos', () => {
+  it('should get all todos', (done) => {
+    request(app)
+      .get('/todos')
+      .expect(200) // 201 failintu, nes siunciam 200
+      .expect((res) => {
+        expect(res.body.todos.length).toBe(2) // 3 failintu, nes idejom tik 2 todos
+      })
+      .end(done);
   });
 });
